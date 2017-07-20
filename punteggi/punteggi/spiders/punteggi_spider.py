72563d710969f40b5d7fc4e121499f7fecdff3cb
La Porta Andrea <ndr.lpr@gmail.com>
2017-07-20 15:34:32 +0200

    Scraping of teams and points

--- punteggi/punteggi/spiders/punteggi_spider.py
+++ punteggi/punteggi/spiders/punteggi_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import pickle
 
 
 class PunteggiSpider(scrapy.Spider):
@@ -6,11 +7,42 @@ class PunteggiSpider(scrapy.Spider):
     start_urls = [
         'http://leghe.fantagazzetta.com/fantascandalo/calendario'
         ]
-
+        
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+        
+        res_punti = []
+        res_teams = []
+        count = 0
+        
+        for day in response.css('div.item'):
+            
+            punti = day.css('table.tbblu').css('tbody').css('td.match')\
+                    .css('span.point::text').extract()
+                   
+            teams = day.css('table.tbblu').css('tbody').css('td.match')\
+                    .css('span').css('a::text').extract()
+                   
+            res_punti += punti
+            res_teams += teams
+            
+        team_names = set(res_teams)
+        fin_res = {i: [] for i in team_names}
+        
+        for team in res_teams:
+            fin_res[team].append(res_punti[count])
+            count += 1
+            
+        f = open('points__names.pckl', 'wb')
+        pickle.dump(fin_res, f)
+        pickle.dump(list(team_names), f)
+        f.close()
+            
+            
+            
+#==============================================================================
+#             yield {
+#                 'text': quote.css('span.text::text').extract_first(),
+#                 'author': quote.css('small.author::text').extract_first(),
+#                 'tags': quote.css('div.tags a.tag::text').extract(),
+#             }
+#==============================================================================
