285b701e3d621345a5c3c2090b7d35c9e8383f60
unknown <ndr.lpr@gmail.com>
2017-07-23 21:59:21 +0200

    Scraping leghe terminato

--- punteggi/punteggi/spiders/classifiche_spider.py
+++ punteggi/punteggi/spiders/classifiche_spider.py
@@ -3,12 +3,18 @@ import pickle
 
 class ClassificheSpider(scrapy.Spider):
     
-    name = 'classifiche'
-    leghe = ['lega--mentirotti', 'fantascandalo', 'diocane']
+    handle_httpstatus_list = [400]
     
+    name = 'classifiche'
+    
+    f = open('1690leghe.pckl', 'rb')
+    leghe = pickle.load(f)
+    f.close()
+
+#    leghe = ['fantasunhopeless']    
         
-    start_urls = ['http://leghe.fantagazzetta.com/%s/classifica' % lega for lega in leghe
-            ]
+    start_urls = ['http://leghe.fantagazzetta.com/%s/classifica'
+                  % lega for lega in leghe]
         
     def parse(self, response):
         
@@ -19,38 +25,40 @@ class ClassificheSpider(scrapy.Spider):
             table = []
             rows = response.css('table').css('tbody').css('tr')
             
-            for row in rows:
-                
-                fantateam = row.css('td')[1].css('a::text').extract_first()
-                pt = int(row.css('td')[2].css('::text').extract_first())
-                g = int(row.css('td')[3].css('::text').extract_first())
-                v = int(row.css('td')[4].css('::text').extract_first())
-                n = int(row.css('td')[5].css('::text').extract_first())
-                p = int(row.css('td')[6].css('::text').extract_first())
-                gf = int(row.css('td')[7].css('::text').extract_first())
-                gs = int(row.css('td')[8].css('::text').extract_first())
-                dr = int(row.css('td')[9].css('::text').extract_first())
-                tot = row.css('td')[10].css('::text').extract_first()
-                tot = float(tot.replace(',','.'))
-                
-                fin_tup = (fantateam,pt,g,v,n,p,gf,gs,dr,tot)
-                
-                table.append(fin_tup)
+            if len(rows) > 0 and len(rows[0].css('td')) == 11:
             
-            try:
-                f = open('classifiche.pckl', 'rb')
-                all_tables = pickle.load(f)
-                f.close()
-                all_tables[nome_lega] = table
-                f = open('classifiche.pckl', 'wb')
-                pickle.dump(all_tables, f)
-                f.close()
+                for row in rows:
+                    
+                    fantateam = row.css('td')[1].css('a::text').extract_first()
+                    pt = int(row.css('td')[2].css('::text').extract_first())
+                    g = int(row.css('td')[3].css('::text').extract_first())
+                    v = int(row.css('td')[4].css('::text').extract_first())
+                    n = int(row.css('td')[5].css('::text').extract_first())
+                    p = int(row.css('td')[6].css('::text').extract_first())
+                    gf = int(row.css('td')[7].css('::text').extract_first())
+                    gs = int(row.css('td')[8].css('::text').extract_first())
+                    dr = int(row.css('td')[9].css('::text').extract_first())
+                    tot = row.css('td')[10].css('::text').extract_first()
+                    tot = float(tot.replace(',','.'))
+                    
+                    fin_tup = (fantateam,pt,g,v,n,p,gf,gs,dr,tot)
+                    
+                    table.append(fin_tup)
                 
-            except FileNotFoundError:
-                all_tables = {nome_lega: table}
-                f = open('classifiche.pckl', 'wb')
-                pickle.dump(all_tables, f)
-                f.close()
+                try:
+                    f = open('classifiche.pckl', 'rb')
+                    all_tables = pickle.load(f)
+                    f.close()
+                    all_tables[nome_lega] = table
+                    f = open('classifiche.pckl', 'wb')
+                    pickle.dump(all_tables, f)
+                    f.close()
+                    
+                except FileNotFoundError:
+                    all_tables = {nome_lega: table}
+                    f = open('classifiche.pckl', 'wb')
+                    pickle.dump(all_tables, f)
+                    f.close()
             
         
         
--- punteggi/test_classifiche.py
+++ punteggi/test_classifiche.py
@@ -1,17 +1,28 @@
 import pickle
 import pandas as pd
 
-f = open('prova_classifiche.pckl', 'rb')
-tutte_le_leghe = pickle.load(f)
+f = open('classifiche.pckl', 'rb')
+tutte_le_classifiche = pickle.load(f)
 f.close()
 
-num_leghe = len(tutte_le_leghe)
+def return_both_class(list_of_tuples):
+    SD = sorted(sorted(list_of_tuples,key=lambda x:x[8],reverse=True),key=lambda x:x[1],reverse=True)
+    SP = sorted(list_of_tuples,key=lambda x:x[9],reverse=True)
+    
+    SD2 = [row[0] for row in SD]
+    SP2 = [row[0] for row in SP]
+    
+    return SD2,SP2
+
+num_leghe = len(tutte_le_classifiche)
 zerocambi = 0
 uncambio = 0
 duecambi = 0
 trecambi = 0
 quattrocambi = 0
 cinquecambi = 0
+vincitore_coincide = 0
+ultimo_vince = 0
 #==============================================================================
 # seicambi = 0
 # settecambi = 0
@@ -19,12 +30,16 @@ cinquecambi = 0
 #==============================================================================
 oltre5 = 0
 
-for lega in tutte_le_leghe:
+for lega in tutte_le_classifiche:
     
     cambi = 0
     
-    class_SD = [row[0] for row in tutte_le_leghe[lega][0]]
-    class_SP = [row[0] for row in tutte_le_leghe[lega][1]]
+    class_SD, class_SP = return_both_class(tutte_le_classifiche[lega])
+    
+    if class_SD[0] == class_SP[0]:
+        vincitore_coincide += 1
+    if class_SD[0] == class_SP[-1]:
+        ultimo_vince += 1
     
     for name in class_SD:
         
@@ -34,6 +49,7 @@ for lega in tutte_le_leghe:
             cambi += 0.5
         elif name != class_SP[indice] and class_SP[class_SD.index(class_SP[indice])] != name:
             cambi += 1
+    
         
     if cambi == 0:
         zerocambi += 1
@@ -64,10 +80,19 @@ results = [zerocambi,uncambio,duecambi,trecambi,quattrocambi,
 first_row = ['Numero di campionati', '%']
 changes = [i for i in range(6)]
 changes.append('>5')
-data = [(i, (i/num_leghe)*100) for i in results]
+data = [(i, round((i/num_leghe)*100,1)) for i in results]
 
 table = pd.DataFrame(data,changes,first_row)
+
+print('\n')
+print('\n')
 print(table)
+print('\n')
+print('Stessa squadra che vince entrambe le classifiche: %d (%d%%).'\
+      % (vincitore_coincide, round((vincitore_coincide/num_leghe)*100,1)))
+#print('\n')
+print('Ultimo in somma punti che vince la classifica a scontri diretti: %d'\
+      '(%d%%).' % (ultimo_vince, round((ultimo_vince/num_leghe)*100,1)))
     
     
     
